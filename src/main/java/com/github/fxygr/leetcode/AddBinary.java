5b379ef138ee0429ee64f2662f1db9a53feead9c
FXYGR <FXYGR@pm.me>
2020-03-03 00:17:18 +0800

    67. Add Binary

--- src/main/java/com/github/fxygr/leetcode/AddBinary.java
+++ src/main/java/com/github/fxygr/leetcode/AddBinary.java
@@ -18,16 +18,16 @@ public class AddBinary {
 			return a;
 		}
 		int maxLength = Math.max(a.length(), b.length());
-		char[] chars = new char[maxLength + 1];
+		short[] chars = new short[maxLength + 1];
 
 		int aIndex = a.length() - 1, bIndex = b.length() - 1;
-		char aChar, bChar, carry = '0';
-		char[] sums;
+		short aShort, bShort, carry = '0';
+		short[] sums;
 		for (int i = 0; i <= maxLength; ++i) {
-			aChar = aIndex < 0 ? '0' : a.charAt(aIndex--);
-			bChar = bIndex < 0 ? '0' : b.charAt(bIndex--);
+			aShort = (short) (aIndex < 0 ? '0' : a.charAt(aIndex--));
+			bShort = (short) (bIndex < 0 ? '0' : b.charAt(bIndex--));
 
-			sums = add(aChar, bChar, carry);
+			sums = add(aShort, bShort, carry);
 			carry = sums[0];
 
 			chars[i] = sums[1];
@@ -39,33 +39,24 @@ public class AddBinary {
 		return s == null || "".equals(s.trim());
 	}
 
-	private String parseString(char[] nums) {
+	private String parseString(short[] nums) {
 		StringBuilder sb = new StringBuilder();
 		int i = nums.length - 1;
 		if (nums[i] == '0') {
 			--i;
 		}
 		for (; i >= 0; --i) {
-			sb.append(nums[i]);
+			sb.append((char) nums[i]);
 		}
 		return sb.toString();
 	}
 
-	private char[] add(char ch1, char ch2, char carry) {
-		char[] chars = null;
-		if (ch1 != ch2) {
-			chars = new char[]{'0', '1'};
-		} else if (ch1 == '0') {
-			chars = new char[]{'0', '0'};
-		} else if (ch1 == '1') {
-			chars = new char[]{'1', '0'};
-		}
-		if (chars[1] != carry) {
-			chars[1] = '1';
-		} else if (carry == '1') {
-			chars[0] = '1';
-			chars[1] = '0';
-		}
+	private short[] add(short ch1, short ch2, short carry) {
+		short[] chars = new short[]{(short) (ch1 & ch2), (short) (ch1 ^ ch2)};
+
+		chars[0] |= (short) (chars[1] & carry);
+		chars[1] ^= carry;
+
 		return chars;
 	}
 
