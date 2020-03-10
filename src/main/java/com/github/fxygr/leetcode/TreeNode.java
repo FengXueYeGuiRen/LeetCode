cea95968c98e9ea476bba3480f1b7d09e2dcd68c
FXYGR <FXYGR@pm.me>
2020-03-10 11:41:26 +0800

    TreeNode

---
+++ src/main/java/com/github/fxygr/leetcode/TreeNode.java
@@ -0,0 +1,18 @@
+package com.github.fxygr.leetcode;
+
+/**
+ * Definition for a binary tree node.
+ *
+ * @author FXYGR @date 2020-03-10
+ */
+public class TreeNode {
+
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	public TreeNode(int x) {
+		val = x;
+	}
+
+}
