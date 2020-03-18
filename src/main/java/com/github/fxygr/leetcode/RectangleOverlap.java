a01bab235761a632e924434a2f422ecd1444d353
FXYGR <FXYGR@pm.me>
2020-03-18 17:54:08 +0800

    836. Rectangle Overlap, optimize

--- src/main/java/com/github/fxygr/leetcode/RectangleOverlap.java
+++ src/main/java/com/github/fxygr/leetcode/RectangleOverlap.java
@@ -11,16 +11,12 @@ package com.github.fxygr.leetcode;
 public class RectangleOverlap {
 
 	public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-		//  x
-		if (((rec1[0] <= rec2[0]) && (rec2[0] < rec1[2])
+		return ((rec1[0] <= rec2[0]) && (rec2[0] < rec1[2])
 				|| (rec2[0] <= rec1[0]) && (rec1[0] < rec2[2]))
 				&&
 				//  y
 				((rec1[1] <= rec2[1]) && (rec2[1] < rec1[3])
-						|| (rec2[1] <= rec1[1]) && (rec1[1] < rec2[3]))) {
-			return true;
-		}
-		return false;
+						|| (rec2[1] <= rec1[1]) && (rec1[1] < rec2[3]));
 	}
 
 }
