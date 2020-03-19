7d22f0aa2b8288bae71c913259f3dc5d825c7ed1
FXYGR <FXYGR@pm.me>
2020-03-19 15:19:11 +0800

    409. Longest Palindrome

---
+++ src/main/java/com/github/fxygr/leetcode/LongestPalindrome.java
@@ -0,0 +1,52 @@
+package com.github.fxygr.leetcode;
+
+import com.github.fxygr.leetcode.utils.StringUtils;
+
+import java.util.Arrays;
+
+/**
+ * 409. Longest Palindrome
+ * (https://leetcode.com/problems/longest-palindrome/)
+ * 409. 最长回文串
+ * (https://leetcode-cn.com/problems/longest-palindrome/)
+ *
+ * @author FXYGR @date 2020-03-19
+ */
+public class LongestPalindrome {
+
+	public int longestPalindrome(String s) {
+		if (StringUtils.isBlank(s)) {
+			return 0;
+		}
+		if (s.length() == 1) {
+			return 1;
+		}
+		int longestPalindrome = 0;
+		int longestOdd = 0;
+		char[] chars = s.toCharArray();
+		Arrays.sort(chars);
+
+		char currentChar = chars[0];
+		int currentCharCount = 1;
+		for (int i = 1; i < s.length(); ++i) {
+			if (chars[i] == currentChar) {
+				++currentCharCount;
+				continue;
+			}
+			if (currentCharCount % 2 == 0) {
+				longestPalindrome += currentCharCount;
+			} else if (currentCharCount > longestOdd) {
+				longestOdd = currentCharCount;
+			}
+			currentChar = chars[i];
+			currentCharCount = 1;
+		}
+		if (currentCharCount % 2 == 0) {
+			longestPalindrome += currentCharCount;
+		} else if (currentCharCount > longestOdd) {
+			longestOdd = currentCharCount;
+		}
+		return longestPalindrome + (longestOdd >= 1 ? longestOdd : 0);
+	}
+
+}
---
+++ src/test/java/com/github/fxygr/leetcode/LongestPalindromeTest.java
@@ -0,0 +1,51 @@
+package com.github.fxygr.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * 409. Longest Palindrome
+ * 409. 最长回文串
+ *
+ * @author FXYGR @date 2020-03-19
+ */
+public class LongestPalindromeTest {
+
+	@InjectMocks
+	private LongestPalindrome longestPalindrome;
+
+	@Before
+	public void setUp() throws Exception {
+		initMocks(this);
+	}
+
+	@Test
+	public void longestPalindrome() {
+		String s = "bb";
+		int longest = longestPalindrome(s);
+		assertEquals(2, longest);
+
+		s = "a";
+		longest = longestPalindrome(s);
+		assertEquals(1, longest);
+
+		s = "abccccdd";
+		longest = longestPalindrome(s);
+		assertEquals(7, longest);
+	}
+
+	private int longestPalindrome(String s) {
+		System.out.println("Input: " + s);
+
+		int longest = longestPalindrome.longestPalindrome(s);
+
+		System.out.println("Output: " + longest);
+		System.out.println();
+		return longest;
+	}
+
+}
