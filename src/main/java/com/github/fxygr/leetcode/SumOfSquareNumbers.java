e7b5882bc3d653fedd55238ce062658c7dde69ae
FXYGR <FXYGR@pm.me>
2020-05-09 16:40:01 +0800

    633. Sum of Square Numbers

---
+++ src/main/java/com/github/fxygr/leetcode/SumOfSquareNumbers.java
@@ -0,0 +1,27 @@
+package com.github.fxygr.leetcode;
+
+/**
+ * 633. Sum of Square Numbers
+ * (https://leetcode.com/problems/sum-of-square-numbers/)
+ * 633. 平方数之和
+ *
+ * @author FXYGR @date 2020-05-09
+ */
+public class SumOfSquareNumbers {
+
+	private ValidPerfectSquare validPerfectSquare = new ValidPerfectSquare();
+
+	public boolean judgeSquareSum(int c) {
+		int aa = c / 2;
+		for (int i = 1; i < aa; ++i) {
+			int a = i * i;
+			int bSquare = c - a;
+			boolean isBPerfectSquare = validPerfectSquare.isPerfectSquare(bSquare);
+			if (isBPerfectSquare) {
+				return isBPerfectSquare;
+			}
+		}
+		return false;
+	}
+
+}
---
+++ src/test/java/com/github/fxygr/leetcode/SumOfSquareNumbersTest.java
@@ -0,0 +1,47 @@
+package com.github.fxygr.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * 633. Sum of Square Numbers
+ * 633. 平方数之和
+ *
+ * @author FXYGR @date 2020-05-09
+ */
+public class SumOfSquareNumbersTest {
+
+	@InjectMocks
+	private SumOfSquareNumbers sumOfSquareNumbers;
+
+	@Before
+	public void setUp() throws Exception {
+		initMocks(this);
+	}
+
+	@Test
+	public void judgeSquareSum() {
+		int c = 5;
+		boolean isSquareSum = judgeSquareSum(c);
+		assertTrue(isSquareSum);
+
+		c = 3;
+		isSquareSum = judgeSquareSum(c);
+		assertFalse(isSquareSum);
+	}
+
+	private boolean judgeSquareSum(int c) {
+		System.out.println("Input: " + c);
+		boolean isSquareSum = sumOfSquareNumbers.judgeSquareSum(c);
+
+		System.out.println("Output: " + isSquareSum);
+		System.out.println();
+		return isSquareSum;
+	}
+
+}
