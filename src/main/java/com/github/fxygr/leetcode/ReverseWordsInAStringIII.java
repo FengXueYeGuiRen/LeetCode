cae09833ae5032009f48544387ea0eb2974ae66e
FXYGR <FXYGR@pm.me>
2020-06-04 16:59:46 +0800

    557. Reverse Words in a String III

---
+++ src/main/java/com/github/fxygr/leetcode/ReverseWordsInAStringIII.java
@@ -0,0 +1,37 @@
+package com.github.fxygr.leetcode;
+
+import com.github.fxygr.leetcode.utils.StringUtils;
+
+/**
+ * 557. Reverse Words in a String III
+ * (https://leetcode.com/problems/reverse-words-in-a-string-iii/)
+ * 557. 反转字符串中的单词 III
+ *
+ * @author FXYGR @date 2020-06-04
+ */
+public class ReverseWordsInAStringIII {
+
+	private static final char CHAR_BLANK = ' ';
+
+	public String reverseWords(String s) {
+		if (StringUtils.isBlank(s)) {
+			return s;
+		}
+		char[] chars = s.trim().toCharArray();
+		for (int i = 0; i < s.length(); ) {
+			int j = i;
+			while (j < s.length() && chars[j] != CHAR_BLANK) {
+				++j;
+			}
+			char temp;
+			for (int k = j - 1; i < k; ++i, --j) {
+				temp = chars[i];
+				chars[i] = chars[k];
+				chars[k] = temp;
+			}
+			i = j + 1;
+		}
+		return String.valueOf(chars);
+	}
+
+}
---
+++ src/test/java/com/github/fxygr/leetcode/ReverseWordsInAStringIIITest.java
@@ -0,0 +1,39 @@
+package com.github.fxygr.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * 557. Reverse Words in a String III
+ * 557. 反转字符串中的单词 III
+ *
+ * @author FXYGR @date 2020-06-04
+ */
+public class ReverseWordsInAStringIIITest {
+
+	@InjectMocks
+	private ReverseWordsInAStringIII reverseWordsInAStringIII;
+
+	@Before
+	public void setUp() throws Exception {
+		initMocks(this);
+	}
+
+	@Test
+	public void reverseWords() {
+	}
+
+	private String reverseWords(String s) {
+		System.out.println("Input: " + s);
+
+		String reverse = reverseWordsInAStringIII.reverseWords(s);
+
+		System.out.println("Output: " + reverse);
+		System.out.println();
+		return reverse;
+	}
+
+}
