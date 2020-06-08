58a68ea679dfde6f9d5490d9af6751655d50efa9
FXYGR <FXYGR@pm.me>
2020-06-08 15:20:33 +0800

    231. Power of Two

---
+++ src/main/java/com/github/fxygr/leetcode/PowerOfTwo.java
@@ -0,0 +1,24 @@
+package com.github.fxygr.leetcode;
+
+/**
+ * 231. Power of Two
+ * (https://leetcode.com/problems/power-of-two/)
+ * 231. 2的幂
+ *
+ * @author FXYGR @date 2020-06-08
+ */
+public class PowerOfTwo {
+
+	public boolean isPowerOfTwo(int n) {
+		if (n == 1 || n == 2) {
+			return true;
+		}
+		for (int i = 2; i < n / 2; i *= 2) {
+			if (i * i == n) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
---
+++ src/test/java/com/github/fxygr/leetcode/PowerOfTwoTest.java
@@ -0,0 +1,60 @@
+package com.github.fxygr.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * 231. Power of Two
+ * 231. 2的幂
+ *
+ * @author FXYGR @date 2020-06-08
+ */
+public class PowerOfTwoTest {
+
+	@InjectMocks
+	private PowerOfTwo powerOfTwo;
+
+	@Before
+	public void setUp() throws Exception {
+		initMocks(this);
+	}
+
+	@Test
+	public void isPowerOfTwo() {
+		int n = 218;
+		boolean isPowerOfTwo = isPowerOfTwo(n);
+		assertFalse(isPowerOfTwo);
+
+		n = 16;
+		isPowerOfTwo = isPowerOfTwo(n);
+		assertTrue(isPowerOfTwo);
+
+		n = 3;
+		isPowerOfTwo = isPowerOfTwo(n);
+		assertFalse(isPowerOfTwo);
+
+		n = 2;
+		isPowerOfTwo = isPowerOfTwo(n);
+		assertTrue(isPowerOfTwo);
+
+		n = 1;
+		isPowerOfTwo = isPowerOfTwo(n);
+		assertTrue(isPowerOfTwo);
+	}
+
+	private boolean isPowerOfTwo(int n) {
+		System.out.println("Input: " + n);
+
+		boolean isPowerOfTwo = powerOfTwo.isPowerOfTwo(n);
+
+		System.out.println("Output: " + isPowerOfTwo);
+		System.out.println();
+		return isPowerOfTwo;
+	}
+
+}
